60df205ae4792047c2059768ee07ebae5a71ec55
Matúš Smolko <matus.smolko@student.tuke.sk>
2020-11-01 11:36:11 +0100

    Přidejte soubory projektu.

---
+++ Actors/Enemy.cs
@@ -0,0 +1,100 @@
+﻿using Merlin.Commands;
+using Merlin2d.Game;
+using Merlin2d.Game.Actions;
+using Merlin2d.Game.Actors;
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace Merlin.Actors
+{
+    public class Enemy : AbstractActor
+    {
+        //Command walking;
+        //Command followPlayer;
+
+        private Animation animationEnemy;
+        private int followNumber = 70;
+        private int positionPlayerX;
+        private int positionPlayerY;
+        private IActor player;
+        private int counter = 0;
+        private int rnd, xEnemy, yEnemy;
+        Random random = new Random();
+
+        public Enemy(IActor player)
+        {
+            animationEnemy = new Animation("resources/enemy.png", 64, 58);
+            SetAnimation(animationEnemy);
+            animationEnemy.Start();
+            SetPosition(250, 120);           
+            //followPlayer = new Move(this, 1, , 0);
+            this.player = player;
+            //move = new Move(this,1,xEnemy,0);
+        }
+
+        public override void Update()
+        {
+            positionPlayerX = player.GetX();
+            positionPlayerY = player.GetY();
+            xEnemy = GetX();
+            yEnemy = GetY();
+
+            if ((SeeNumber(positionPlayerX, xEnemy, positionPlayerY, yEnemy) <= followNumber) && (counter % 30 == 0))
+            {
+                new Move(this, 1, Follow(positionPlayerX, xEnemy, positionPlayerY, yEnemy), 0).Execute();
+                Console.WriteLine("Enemy see player!");
+            }
+            else
+            {
+                if (counter % 60 == 0)
+                {
+                    rnd = random.Next(-10, 10);
+                    new Move(this, 1, rnd, 0).Execute();
+                }
+            }
+            if(positionPlayerX == this.GetX())
+            {
+                Console.WriteLine("Merlin is caught!");
+            }
+            counter++;
+        }
+
+        private int SeeNumber(int xP, int xE, int yP, int yE)
+        {
+            int finalNumber,dimenseFirst,dimenseSecond;
+
+            dimenseFirst = xP - xE;
+            dimenseSecond = yP - yE;
+
+            if(dimenseFirst <= 0)
+            {
+                finalNumber = dimenseFirst * (-1);
+            }
+            else
+            {
+                finalNumber = dimenseFirst;
+            }
+            return finalNumber;
+        }
+                  
+        private int Follow(int xP, int xE, int yP, int yE)
+        {
+            int finalNumber, left, right , up, down;
+
+           if(xP - xE >= 0)
+            {
+                finalNumber = 3;
+            }
+            else
+            {
+                finalNumber = -3;
+            }
+            return finalNumber;
+        }
+    }
+}
+
+
+
---
+++ Actors/Player.cs
@@ -0,0 +1,109 @@
+﻿using Merlin.Commands;
+using Merlin2d.Game;
+using Merlin2d.Game.Actions;
+using Merlin2d.Game.Actors;
+using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Merlin.Actors
+{
+    public class Player : AbstractActor, IMovable
+    {
+        Animation animation_MerlinWalking = new Animation("resources/player.png", 64, 58);
+        Command moveLeft;
+        Command moveRight;
+        //Command moveUp;
+        Command moveDown;
+        int counterHop = 1;
+       
+        //int playerX;
+        //int playerY;
+        // bool colision = false;
+
+        public Player()
+        {
+            animation_MerlinWalking.Start();
+            SetAnimation(animation_MerlinWalking);
+            SetPosition(250, 200);           
+        }
+
+        public override void Update()
+        {
+            moveLeft = new Move(this, 1, -1, 0);
+            moveRight = new Move(this, 1, 1, 0);
+            new Jump(this, 3, 10, -7);
+            moveDown = new Move(this, 1, 0, 1);
+
+            if (Input.GetInstance().IsKeyDown(Input.Key.LEFT) == true)
+            {
+                animation_MerlinWalking.Start();
+                if (Input.GetInstance().IsKeyDown(Input.Key.SPACE) == true)
+                {
+                    moveLeft.Execute();
+                    new Jump(this, 3, 10, -1).Execute();
+                }
+                else
+                {
+                    moveLeft.Execute();
+                }
+            }
+            else if (Input.GetInstance().IsKeyDown(Input.Key.RIGHT) == true)
+            {
+                animation_MerlinWalking.Start();
+                if (Input.GetInstance().IsKeyPressed(Input.Key.SPACE) == true)
+                {
+                    moveRight.Execute();
+                    new Jump(this, 3, 10, 1).Execute();
+                }                
+                else
+                {
+                    moveRight.Execute();
+                }
+            }
+           else if ((Input.GetInstance().IsKeyDown(Input.Key.SPACE) == true) )
+            {
+                animation_MerlinWalking.Start();
+                new Jump(this, 3, 0, 0).Execute();
+                
+            }
+           /* else if ((Input.GetInstance().IsKeyDown(Input.Key.D) == true) )
+            {
+                animation_MerlinWalking.Start();
+                new Jump(this, 3, 10, 1).Execute();
+                moveLeft.Execute();
+            }
+            else if ((Input.GetInstance().IsKeyDown(Input.Key.A) == true) )
+            {
+                animation_MerlinWalking.Start();
+                new Jump(this, 3, 10, -1).Execute();
+                moveRight.Execute();
+            }*/
+            else if (Input.GetInstance().IsKeyDown(Input.Key.DOWN) == true)
+            {
+                animation_MerlinWalking.Start();
+                moveDown.Execute();
+            }
+            else
+            {
+                animation_MerlinWalking.Stop();
+            }            
+        }
+        public int PlayerCoordinatX
+        {
+            get
+            {
+                return GetX();
+            }
+        }
+
+        public int PlayerCoordinatY
+        {
+            get
+            {
+                return GetY(); 
+            }
+        }
+    }
+}
